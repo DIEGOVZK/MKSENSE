faca2777d1cc7e37d2bd98c3e5af870c4311282e
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-03-21 18:45:13 -0300

    Working Example

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
-import LocalServer from "./LocalServer.mjs";
+import LocalServer from "./LocalServer.js";
 
+// Cria objetos que instanciam servidores locais
 var server_1 = new LocalServer(8080); 
+var server_2 = new LocalServer(); 
+
+// Inicialisa os servidores criados
 server_1.open();
+server_2.open();
