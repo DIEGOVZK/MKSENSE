db5a9fc2c7fad565ff8ca0631dae017d80274610
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-03-21 21:54:16 -0300

    Working prototype

--- src/LocalServer.js
+++ src/LocalServer.js
@@ -1,9 +1,13 @@
+// Dependências - http, 
 import http from 'http';
 
 /**
- * @class LocalServer
- * @abstract Cria um objeto servidor responsável por se comunicar com um cliente
- *           via requisições HTTP. Cada objeto será reconhecido pelo seu PORT único de comunicação
+ * @class       LocalServer
+ * @public 
+ * @exports     class
+ * @constructor Recebe opcionalmente 'PORT:n' a ser anexado com o servidor 
+ * @abstract    Cria um objeto servidor responsável por se comunicar com um cliente
+ *              via requisições HTTP. Cada objeto será reconhecido pelo seu PORT único de comunicação
  */
 export default class LocalServer {
 
@@ -12,26 +16,23 @@ export default class LocalServer {
     port;
 
     // Inicializa o identificador de classes
-    static _identifier;
-    _identifier = 0;
-
+    static _identifier = 0;
 
     // Construtor da classe LocalServer
     constructor(port) {
-        
+
         // Cria objeto passando PORT
         if (!arguments.length) {
-            this._identifier++;
-            this.port = this._identifier;
-        }
-        // Cria objeto usando identificador como PORT
-        else {
-            this.port = port;
-            this._identifier++;
+            LocalServer._identifier++;
+            this.port = LocalServer._identifier;
         }
 
-        /* Foi feito assim pois javascript não permite 
-        sobrecarga de construtories e métodos */
+        // Cria objeto usando identificador como PORT
+        else
+            this.port = port;
+
+        /* Foi feito assim pois javascript é chato e
+        não permite sobrecarga de construtories e métodos */
     }
 
 
@@ -40,12 +41,12 @@ export default class LocalServer {
      * @abstract Inicia servidor HTTP na porta escolhida na classe LocalServer,
      *           recebe os pacotes HTTP e traduz para formato JSON antes de enviar ao servidor
      * 
-     * @access public
+     * @public
      */
     open() {
 
         // Variável para armazenar pacote recebido
-        var _bodyBuffer = "";
+        var _bodyBuffer = "default";
 
         // Cria objeto Server usando bib http
         const app = http.createServer();
@@ -68,7 +69,7 @@ export default class LocalServer {
                     this._jsonBuffer = JSON.parse(_bodyBuffer);
 
                     // Mostra os dados recebidos na tela
-                    console.log(`Body Contents: ${_bodyBuffer}`);
+                    console.log(`\rRecebeu ${_bodyBuffer.length}` + " chars no port " + this.port);
 
                 } catch (error) {
                     console.log("Erro ao montar JSON no port: " +
@@ -85,53 +86,3 @@ export default class LocalServer {
         app.listen(this.port);
     }
 }
-
-
-/*
-
-// Configura porta para comunicação
-const port = 8080;
-
-// Importa a bib http para req handling
-const http = require("http");
-
-// Cria objeto Server usando bib http
-const app = http.createServer();
-
-// No evento de requisição HTTP
-app.on("request", (req, res) => {
-
-    // Cria _bodyBuffer que recebe as chuncks
-    var _bodyBuffer = [];
-    var _jsonBuffer = "";
-
-    // No recebimento junta as chuncks no _bodyBuffer
-    req.on("data", (bodyData) => {
-        _bodyBuffer.push(bodyData);
-    });
-
-    // Ao fim da transmissão, contatena tudo e converte...
-    req.on("end", () => {
-
-        try {
-
-            // Converte buffer para String e Json object
-            _bodyBuffer = Buffer.concat(_bodyBuffer).toString();
-            _jsonBuffer = JSON.parse(_bodyBuffer);
-
-            // Mostra os dados recebidos na tela
-            console.log(`Body Contents: ${_bodyBuffer}`);
-
-        } catch (error) { console.log("Erro ao montar JSON: " + error); }
-
-    });
-
-    // Responde 200 OK
-    res.end(200);
-});
-
-// Inicia servidor e 'escuta' na porta especificada
-console.log(`Server started on port: ${port}`);
-app.listen(port);
-
-*/
--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import LocalServer from "./LocalServer.js";
 // Cria objetos que instanciam servidores locais
 var server_1 = new LocalServer(8080); 
 var server_2 = new LocalServer(); 
+var server_3 = new LocalServer(); 
 
 // Inicialisa os servidores criados
 server_1.open();
 server_2.open();
+server_3.open();
