97c082fd934a0a8c7e8b0c8edc53b4413485298f
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-03-24 17:05:26 -0300

    Working CSV and Saving all data

--- src/LocalServer.js
+++ src/LocalServer.js
@@ -1,5 +1,6 @@
 // Dependências - http, 
 import http from 'http';
+import fs from 'fs';
 
 /**
  * @class       LocalServer
@@ -68,15 +69,24 @@ export default class LocalServer {
                     _bodyBuffer = Buffer.concat(_bodyBuffer).toString();
                     this._jsonBuffer = JSON.parse(_bodyBuffer);
 
-                    // Mostra os dados recebidos na tela
-                    process.stdout.clearLine(0);
-                    process.stdout.cursorTo(0);
-                    process.stdout.write(`\x1B[32m Recebeu ${_bodyBuffer.length}` +
+                    // TODO: Alterar para interação com o banco de dados
+                    fs.appendFile('SensorialData.csv', (this.port + ", " + _bodyBuffer) + '\n',
+                        function (err) {
+                            if (err) {
+                                process.stdout.write("\x1B[31m Erro ao salvar dados do port: " +
+                                    this.port + " Erro: " + err + "\n");
+                            }
+                        });
+
+                        // Mostra os dados recebidos na tela
+                        process.stdout.clearLine(0);
+                        process.stdout.cursorTo(0);
+                        process.stdout.write(`\x1B[32m Recebeu ${_bodyBuffer.length}` +
                         " chars no port " + this.port);
-
-                    // Reseta o  buffer para a próxima execução
-                    _bodyBuffer = [];
-
+                        
+                        // Reseta o  buffer para a próxima execução
+                        _bodyBuffer = [];
+                        
                     // Captura erro em caso do JSON não esteja formatado
                 } catch (error) {
                     process.stdout.clearLine(0);
