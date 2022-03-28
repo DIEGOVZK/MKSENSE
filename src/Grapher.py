402cf11dce1786055a6e43314171d3f5f1180377
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-03-28 18:13:59 -0300

    included example

---
+++ src/Grapher.py
@@ -0,0 +1,11 @@
+# Dependências
+import matplotlib as plt
+
+# Exemplo ------------------------------------------
+# Amostragem dos vetores R e C da imagem
+# plt.plot(X_arr, 'r', label='R profiling (x axis)')
+# plt.plot(Y_arr, 'k', label='C profiling (y axis)')
+# plt.legend()
+# plt.xlabel("Pixel Position")
+# plt.ylabel("Pixel Intensity")
+# plt.show()
