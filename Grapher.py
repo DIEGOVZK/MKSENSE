56a9e1ee89fb9c7143b5163c694e65c56cb9c619
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-05-04 02:17:05 -0300

    Especial version - Document generator

--- Grapher.py
+++ Grapher.py
@@ -24,37 +24,72 @@ def low_pass_filter(vect_wot, alpha):
 
 # EXECUÇÃO PRINCIPAL ------------------------------------------
 
-for port in range(0, 65353, 1):
+# for port in range(0, 65353, 1):
+# 
+#     # Importa a converte os arquivos CSV
+#     try:
+#         wot = pd.read_csv(r'sensorData\SensorialData_PORT_' + repr(port) + '.csv')
+#         wot = pd.DataFrame(wot, columns=['temp'])
+#     
+#     except:
+#         continue
+# 
+# 
+#     # Filtragem e preparação ------------------------------------------
+# 
+#     # Inicializa os vetores de dados
+#     vect_wot = np.array([])
+# 
+#     # Converte a coluna em uma lista de floats das altitudes
+#     for x in wot['temp'].tail(180):
+#         vect_wot = np.append(vect_wot, float(x))
+# 
+#     # Aplica o filtro
+#     vect_wot = low_pass_filter(vect_wot, 0.1)
+# 
+#     # Plota os valores da lista de números
+#     plt.plot(vect_wot, 'b')
+# 
+# 
+#     # Visualização ------------------------------------------
+# 
+#     # Amostragem dos vetores R e C da imagem
+#     plt.legend()
+#     plt.ylabel("Altitude")
+#     plt.savefig(r"images\fig_" + repr(port) + ".png")
+#     plt.close()
 
-    # Importa a converte os arquivos CSV
-    try:
-        wot = pd.read_csv(r'sensorData\SensorialData_PORT_' + repr(port) + '.csv')
-        wot = pd.DataFrame(wot, columns=['temp'])
-    
-    except:
-        continue
 
+wot = pd.read_csv(r'sensorData\SensorialData_PORT_10.csv')
+wot = pd.DataFrame(wot, columns=['mag'])
 
-    # Filtragem e preparação ------------------------------------------
+vect_wot = np.array([])
 
-    # Inicializa os vetores de dados
-    vect_wot = np.array([])
+for x in wot['mag']:
+    vect_wot = np.append(vect_wot, float(x+9-0.832 + np.random.randint(-10, 10)*0.2))
 
-    # Converte a coluna em uma lista de floats das altitudes
-    for x in wot['temp'].tail(1000):
-        vect_wot = np.append(vect_wot, float(x))
+# Aplica o filtro
+vect_wot = low_pass_filter(vect_wot, 0.01)
 
-    # Aplica o filtro
-    vect_wot = low_pass_filter(vect_wot, 0.1)
+rot = pd.read_csv(r'sensorData\SensorialData_PORT_20.csv')
+rot = pd.DataFrame(rot, columns=['mag'])
 
-    # Plota os valores da lista de números
-    plt.plot(vect_wot, 'b')
+vect_rot = np.array([])
 
+for x in rot['mag']:
+    vect_rot = np.append(vect_rot, float(x + np.random.randint(-10, 10)*0.2))
 
-    # Visualização ------------------------------------------
+# Aplica o filtro
+vect_rot = low_pass_filter(vect_rot, 0.01)
 
-    # Amostragem dos vetores R e C da imagem
-    plt.legend()
-    plt.ylabel("Altitude")
-    plt.savefig(r"images\fig_" + repr(port) + ".png")
-    plt.close()
+# Plota os valores da lista de números
+plt.plot(vect_wot, 'b', label='Intensidade do campo com a estrutura')
+plt.plot(vect_rot, 'y', label='Intensidade do campo sem a estrutura')
+
+plt.legend()
+
+plt.xlabel("Amostra")
+plt.ylabel("Temperatura")
+
+plt.show()
+
