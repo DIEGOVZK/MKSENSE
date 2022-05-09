c8d337937b658a18c5d5c30681ae0addec127193
Diego Anestor Coutinho <diegoacoutinho1224@gmail.com>
2022-05-09 10:55:09 -0300

    Removed References and Cleanup

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@
 import LocalServer from "./LocalServer";
 
 // Cria objeto localServer
-const localServer = new LocalServer(343);
-const localServer2 = new LocalServer(402);
+// const localServer = new LocalServer(343);
+// const localServer2 = new LocalServer(402);
+const localServer3 = new LocalServer(80);
 
 // Inicia o servidor
-localServer.iniciarServidor();
-localServer2.iniciarServidor();
+// localServer.iniciarServidor();
+// localServer2.iniciarServidor();
+localServer3.iniciarServidor();
